3485e3cb9bbd599016b3cac629973effddfb613c
Aloosh966 <ali.al-@graduate.utm.my>
2025-06-16 22:54:39 +0800

    fixed errors in admin

--- lib/screens/admin/analytics/admin_analytics_screen.dart
+++ lib/screens/admin/analytics/admin_analytics_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import '../routes/app_routes.dart';
+import '../../../routes/app_routes.dart';
 
 class AdminAnalyticsScreen extends StatefulWidget {
   const AdminAnalyticsScreen({super.key});
